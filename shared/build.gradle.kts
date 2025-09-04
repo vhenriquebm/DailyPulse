46408e116a0d47a062852c16228bdb42d086f15c
Vitor Marinho <89821794+vhenriquebm@users.noreply.github.com>
2025-09-04 15:09:05 +0100

    Add date formatter

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -33,6 +33,7 @@ kotlin {
                 implementation("io.ktor:ktor-client-content-negotiation:3.2.3")
                 implementation("io.ktor:ktor-serialization-kotlinx-json:3.2.3")
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+                implementation(libs.kotlinx.datetime)
             }
         }
 
--- shared/src/commonMain/kotlin/com/example/dailypulse/articles/ArticleUseCase.kt
+++ shared/src/commonMain/kotlin/com/example/dailypulse/articles/ArticleUseCase.kt
@@ -1,5 +1,13 @@
 package com.example.dailypulse.articles
 
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.daysUntil
+import kotlinx.datetime.toLocalDateTime
+import kotlinx.datetime.todayIn
+import kotlin.math.abs
+
 class ArticleUseCase(private val service: ArticleService) {
 
     suspend fun getArticles(): List<Article> {
@@ -11,8 +19,23 @@ class ArticleUseCase(private val service: ArticleService) {
         Article(
             raw.title,
             raw.desc ?: "Click to find out more",
-            raw.date,
+            getDaysAgoString(raw.date),
             raw.imageUrl ?: ""
         )
+    }
+
+    private fun getDaysAgoString(date: String): String {
+        val today = Clock.System.todayIn(TimeZone.currentSystemDefault())
+        val days = today.daysUntil(
+            Instant.parse(date).toLocalDateTime(TimeZone.currentSystemDefault()).date
+        )
+
+        val result = when {
+            abs(days) > 1 -> "${abs(days)} days ago"
+            abs(days) == 1 -> "Yesterday"
+            else -> "Today"
+        }
+
+        return result
     }
 }
