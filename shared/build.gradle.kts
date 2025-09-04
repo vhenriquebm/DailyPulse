034e0350424dd36faa4ab43b58518117fbcda368
Vitor Marinho <89821794+vhenriquebm@users.noreply.github.com>
2025-09-04 14:14:13 +0100

    Fix build.gradle.kts issues

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,8 +4,8 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinSerialization)
     id("co.touchlab.skie") version "0.10.6"
-    kotlin("plugin.serialization") version "1.9.20"
 }
 
 kotlin {
@@ -14,7 +14,11 @@ kotlin {
         compilerOptions { jvmTarget.set(JvmTarget.JVM_11) }
     }
 
-    listOf(iosX64(), iosArm64(), iosSimulatorArm64()).forEach { iosTarget ->
+    val iosX64Target = iosX64()
+    val iosArm64Target = iosArm64()
+    val iosSimArm64Target = iosSimulatorArm64()
+
+    listOf(iosX64Target, iosArm64Target, iosSimArm64Target).forEach { iosTarget ->
         iosTarget.binaries.framework {
             baseName = "Shared"
             isStatic = true
@@ -30,29 +34,40 @@ kotlin {
                 implementation("io.ktor:ktor-serialization-kotlinx-json:3.2.3")
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
             }
+        }
 
-            val androidMain by getting {
-                dependencies {
-                    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.1")
-                    implementation(libs.ktor.client.okhttp)
-                }
+        val androidMain by getting {
+            dependencies {
+                implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.1")
+                implementation(libs.ktor.client.okhttp)
             }
+        }
 
-            val commonTest by getting {
-                dependencies { implementation(libs.kotlin.test) }
-            }
+        val iosMain by creating { dependsOn(commonMain) }
+        val iosX64Main by getting { dependsOn(iosMain) }
+        val iosArm64Main by getting { dependsOn(iosMain) }
+        val iosSimulatorArm64Main by getting { dependsOn(iosMain) }
+
+        iosMain.dependencies {
+            implementation("io.ktor:ktor-client-darwin:3.2.3")
+        }
+
+        val commonTest by getting {
+            dependencies { implementation(libs.kotlin.test) }
         }
     }
+}
 
-    android {
-        namespace = "com.example.dailypulse.shared"
-        compileSdk = libs.versions.android.compileSdk.get().toInt()
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_11
-            targetCompatibility = JavaVersion.VERSION_11
-        }
-        defaultConfig {
-            minSdk = libs.versions.android.minSdk.get().toInt()
-        }
+android {
+    namespace = "com.example.dailypulse"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 }
